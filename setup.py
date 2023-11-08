e2b1d7dc1940996df2aae1d339fd5e52e6408570
nplak <n.plaksin8@gmail.com>
2023-11-08 23:58:21 +0300

    add setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="diffuse_animation",
+    packages=find_packages(),
+    version="0.0.1",
+    description="test",
+    author="runny",
+    license="",
+)
